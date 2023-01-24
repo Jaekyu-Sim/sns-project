dc163e9955fb967bdcd5558f7ee9026e33d53c6c
Jaekyu Sim <worb1605@naver.com>
2023-01-25 00:57:33 +0900

    01/25 - lecture 작성

---
+++ src/main/java/com/fastcampus/snsproject/controller/request/UserJoinRequest.java
@@ -0,0 +1,14 @@
+package com.fastcampus.snsproject.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class UserJoinRequest {
+
+    private String userName;
+    private String password;
+
+}
