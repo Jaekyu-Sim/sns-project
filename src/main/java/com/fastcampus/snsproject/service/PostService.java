a940815cb019be9bf89d381e80c4b1000e8d40f4
Jaekyu Sim <worb1605@naver.com>
2023-01-26 00:57:13 +0900

    01/26 lecture02 finish. feed 조회 / my feed 조회 기능 추가

--- src/main/java/com/fastcampus/snsproject/service/PostService.java
+++ src/main/java/com/fastcampus/snsproject/service/PostService.java
@@ -8,6 +8,8 @@ import com.fastcampus.snsproject.model.entity.UserEntity;
 import com.fastcampus.snsproject.repository.PostEntityRepository;
 import com.fastcampus.snsproject.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -59,4 +61,19 @@ public class PostService {
 
         postEntityRepository.delete(postEntity);
     }
+
+
+    public Page<Post> list(Pageable pageable)
+    {
+        return postEntityRepository.findAll(pageable).map(Post::fromEntity);
+    }
+
+    public Page<Post> my(String userName, Pageable pageable)
+    {
+        //user find
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(()->new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+
+        return postEntityRepository.findAllByUser(userEntity, pageable).map(Post::fromEntity);
+    }
+
 }
